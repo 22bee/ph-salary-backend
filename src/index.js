829bb776ec58b2edb8925eaef61c3ea9d664864b
22bee <93537986+22bee@users.noreply.github.com>
2025-11-28 14:43:57 +0800

    Update index 3.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',                 // local dev
-    'https://your-frontend-url.vercel.app'   // replace with your Vercel URL
+    'https://ph-salary-frontend-tim.vercel.app'   // replace with your Vercel URL
   ]
 }));
 
